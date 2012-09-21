77594efd89cea37671cd6688fc7f0b86233b533c
paraiko <paraiko@gmail.com>
2012-09-21 19:03:55 +0200

    look for investigation name system instead of first investigation of logged in user.

--- apps/animaldb/org/molgenis/animaldb/plugins/settings/BackgroundPlugin.java
+++ apps/animaldb/org/molgenis/animaldb/plugins/settings/BackgroundPlugin.java
@@ -20,29 +20,26 @@ import org.molgenis.pheno.ObservationTarget;
 import org.molgenis.util.Entity;
 import org.molgenis.util.Tuple;
 
-
-public class BackgroundPlugin extends PluginModel<Entity>
-{
+public class BackgroundPlugin extends PluginModel<Entity> {
 	private static final long serialVersionUID = 6637437260773077373L;
 	private List<ObservationTarget> backgroundList;
 	private List<ObservationTarget> speciesList;
 	private String action = "init";
 	private CommonService ct = CommonService.getInstance();
 	private Map<Integer, String> speciesMap;
-	
-	public BackgroundPlugin(String name, ScreenController<?> parent)
-	{
+
+	public BackgroundPlugin(String name, ScreenController<?> parent) {
 		super(name, parent);
 	}
-	
-	public String getCustomHtmlHeaders()
-    {
+
+	public String getCustomHtmlHeaders() {
 		return "<link rel=\"stylesheet\" style=\"text/css\" href=\"res/css/animaldb.css\">";
-    }
-	
+	}
+
 	public List<ObservationTarget> getBackgroundList() {
 		return backgroundList;
 	}
+
 	public void setBackgroundList(List<ObservationTarget> backgroundList) {
 		this.backgroundList = backgroundList;
 	}
@@ -56,57 +53,59 @@ public class BackgroundPlugin extends PluginModel<Entity>
 	}
 
 	@Override
-	public String getViewName()
-	{
+	public String getViewName() {
 		return "org_molgenis_animaldb_plugins_settings_BackgroundPlugin";
 	}
 
 	@Override
-	public String getViewTemplate()
-	{
+	public String getViewTemplate() {
 		return "org/molgenis/animaldb/plugins/settings/BackgroundPlugin.ftl";
 	}
 
-	public String getAction()
-	{
+	public String getAction() {
 		return action;
 	}
 
-	public void setAction(String action)
-	{
+	public void setAction(String action) {
 		this.action = action;
 	}
-	
+
 	public String getSpecies(int backgroundId) {
 		return speciesMap.get(backgroundId);
 	}
 
 	@Override
-	public void handleRequest(Database db, Tuple request)
-	{
+	public void handleRequest(Database db, Tuple request) {
 		ct.setDatabase(db);
 		try {
 			action = request.getString("__action");
-			
+
 			if (action.equals("Add")) {
 				//
 			}
-			
+
 			if (action.equals("Import")) {
 				//
 			}
-			
+
 			if (action.equals("addBackground")) {
 				String bkgName = request.getString("name");
 				String speciesName = request.getString("species");
-				String investigationName = ct.getOwnUserInvestigationName(this.getLogin().getUserName());
-				ct.makePanel(investigationName, bkgName, this.getLogin().getUserName());
-				db.add(ct.createObservedValueWithProtocolApplication(investigationName, new Date(), null, 
-						"SetTypeOfGroup", "TypeOfGroup", bkgName, "Background", null));
-				db.add(ct.createObservedValueWithProtocolApplication(investigationName, new Date(), null, 
-						"SetSpecies", "Species", bkgName, null, speciesName));
+				// String investigationName =
+				// ct.getOwnUserInvestigationName(this.getLogin().getUserName());
+				String investigationName = "System"; // TODO remove old
+														// investigation code
+														// above.
+				ct.makePanel(investigationName, bkgName, this.getLogin()
+						.getUserName());
+				db.add(ct.createObservedValueWithProtocolApplication(
+						investigationName, new Date(), null, "SetTypeOfGroup",
+						"TypeOfGroup", bkgName, "Background", null));
+				db.add(ct.createObservedValueWithProtocolApplication(
+						investigationName, new Date(), null, "SetSpecies",
+						"Species", bkgName, null, speciesName));
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			if (e.getMessage() != null) {
@@ -115,23 +114,25 @@ public class BackgroundPlugin extends PluginModel<Entity>
 		}
 	}
 
-	public void reload(Database db)
-	{
+	public void reload(Database db) {
 		ct.setDatabase(db);
-		
+
 		// Populate background and species list/map
 		try {
-			List<String> investigationNames = ct.getAllUserInvestigationNames(this.getLogin().getUserName());
-			backgroundList = ct.getAllMarkedPanels("Background", investigationNames);
+			List<String> investigationNames = ct
+					.getAllUserInvestigationNames(this.getLogin().getUserName());
+			backgroundList = ct.getAllMarkedPanels("Background",
+					investigationNames);
 			speciesList = ct.getAllMarkedPanels("Species", investigationNames);
 			speciesMap = new HashMap<Integer, String>();
 			for (ObservationTarget background : backgroundList) {
-				String speciesName = ct.getMostRecentValueAsXrefName(background.getName(), "Species");
+				String speciesName = ct.getMostRecentValueAsXrefName(
+						background.getName(), "Species");
 				if (speciesName != null) {
 					speciesMap.put(background.getId(), speciesName);
 				}
 			}
-			
+
 		} catch (Exception e) {
 			String message = "Something went wrong while loading backgrounds and species";
 			if (e.getMessage() != null) {
@@ -141,5 +142,5 @@ public class BackgroundPlugin extends PluginModel<Entity>
 			e.printStackTrace();
 		}
 	}
-	
+
 }
